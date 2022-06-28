808f1ded0102f74dff87b69a6f5fc3ae7c47e530
Bruno Lamps <lamparolho@gmail.com>
2022-06-28 10:38:17 -0300

    Nuxt 3 installed. I'm toying around a menu always in the smaller screen dimension.

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    <TheHeader></TheHeader>
+    <NuxtPage />
+  </div>
+</template>
+<style scoped>
+div.main {
+  display: flex;
+  flex-direction: column;
+}
+
+@media screen and (orientation: landscape) {
+  div.main {
+    flex-direction: row;
+  }
+}
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <menu>
+        <li>logo</li>
+        <li>title</li>
+        <li>Apps</li>
+        <li>Contact</li>
+    </menu>
+</template>
+<style scoped>
+menu {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    justify-content: space-evenly;
+}
+
+@media screen and (orientation: landscape) {
+    menu {
+        flex-direction: column;
+    }
+}
+</style>
