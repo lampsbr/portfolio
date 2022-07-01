52d591c9c27a2b270767426d03e834e258749ce8
Bruno Lamps <lamparolho@gmail.com>
2022-07-01 17:38:56 -0300

    add react-bootstrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Button } from 'react-bootstrap'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -16,6 +17,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Button variant="primary">my button</Button>
 
         <p className={styles.description}>
           Get started by editing{' '}
